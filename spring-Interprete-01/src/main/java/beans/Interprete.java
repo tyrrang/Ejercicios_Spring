cadd8d8ffa0366c848c448c1e54c5f721118ff3c
Julio CR <tyrrang@gmail.com>
2016-11-07 13:45:59 -0600

    Add files via upload

---
+++ spring-Interprete-01/src/main/java/beans/Interprete.java
@@ -0,0 +1,6 @@
+package beans;
+
+public interface Interprete {
+	public void saludar();
+	public void despedirse();
+}
---
+++ spring-Interprete-01/src/main/java/beans/InterpreteIngles.java
@@ -0,0 +1,11 @@
+package beans;
+
+public class InterpreteIngles implements Interprete {
+	public void saludar() {
+		System.out.println("Hello, my name is ");
+	}
+
+	public void despedirse() {
+		System.out.println("Bye...");
+	}
+}
