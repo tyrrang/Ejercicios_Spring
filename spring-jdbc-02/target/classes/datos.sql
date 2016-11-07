e54fc3857aee06f3131d561512d444c5e03bc6b1
Julio CR <tyrrang@gmail.com>
2016-11-07 13:34:20 -0600

    Add files via upload

---
+++ spring-jdbc-02/target/classes/datos.sql
@@ -0,0 +1,3 @@
+insert into persona (nombre, ape_paterno, email) values ('Admin', 'admin', 'admin@icursos.net');
+insert into persona (nombre, ape_paterno, email) values ('Juan', 'Perez', 'juan@gmail.com');
+insert into persona (nombre, ape_paterno, email) values ('Juan', 'Rodriguez', 'jrodriguez@hotmail.com');
